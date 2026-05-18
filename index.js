d109584274442fedd5f787d4dcafb560cb2cb051
Caleb Busolo <wabombacaleb@gmail.com>
2026-05-18 16:51:59 +0300

    Update index.js with latest backend changes

--- index.js
+++ index.js
@@ -94,14 +94,8 @@ app.post(
 
 if (event.event !== "charge.success" || event.data?.status !== "success") return;
 
-// ignore ONLY subscription payments
-const isSubscription =
-  event.data?.metadata?.type === "subscription";
-
-if (isSubscription) {
-  log("⏭ Subscription bypassed Node.js");
-  return;
-}
+// ignore subscription payments completely
+if (event.data?.metadata?.type !== "credits") return;
 
 const { reference, customer, amount } = event.data || {};
 
